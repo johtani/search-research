b6e75b1d641bbea74ff57d5914020929d70d2c42
Jun Ohtani <johtani@gmail.com>
2023-07-20 14:27:22 +0900

    ワークスペース用のコンテナを、docker-compose.ymlベースに切り替え

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,8 +7,11 @@ sudo apt update
 sudo apt-get install cmake -y
 sudo chown -R vscode:vscode .
 
+pip install poetry
+
 poetry config virtualenvs.in-project true
 poetry install --no-root
 pip install git+https://github.com/rinnakk/japanese-clip.git
+poetry run pre-commit install
 
 echo "FINISH Install"
