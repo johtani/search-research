1f56d3ee6c0a066cd12fd1e63c0053e995ab0972
Jun Ohtani <johtani@gmail.com>
2023-07-25 01:03:51 +0900

    Add source activate for using   pre-commit with VS Code Source Control

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -14,5 +14,7 @@ poetry config virtualenvs.in-project true
 poetry install --no-root
 poetry run pip install git+https://github.com/rinnakk/japanese-clip.git
 poetry run pre-commit install
+VENV=`poetry env info -p`
+echo "source ${VENV}/bin/activate" >> ~/.bashrc
 
 echo "FINISH Install"
