750a2813d749b4d61d3149b3f1bf145da27d6d99
Jun Ohtani <johtani@gmail.com>
2023-09-01 01:59:44 +0900

    vespa-cliの追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,6 +9,13 @@ sudo apt-get install git-lfs
 sudo apt-get install zstd
 sudo chown -R vscode:vscode .
 
+# install vespa-cli
+wget -O /tmp/vespa-cli.tgz https://github.com/vespa-engine/vespa/releases/download/v8.218.31/vespa-cli_8.218.31_linux_amd64.tar.gz
+tar zxf /tmp/vespa-cli.tgz -C /tmp
+sudo mv /tmp/vespa-cli_8.218.31_linux_amd64 /usr/local/vespa 
+echo 'export PATH="/usr/local/vespa/bin:$PATH"' >> ~/.bashrc
+
+# for python 
 pip install poetry
 
 poetry config virtualenvs.in-project true
